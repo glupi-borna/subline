f9a818018d9450341fda7e5fd77d52da3217396b
Borna Lang <borna.lang@gmail.com>
2023-12-06 02:18:00 +0100

    Work

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <dirent.h>
 #include <limits.h>
 #include <unistd.h>
+#include <malloc.h>
+#include <stdlib.h>
 
 struct string {
     char* text;
@@ -28,12 +30,19 @@ string to_string(char* ch) {
     return out;
 }
 
+string copy(string str) {
+    auto data = (char*)malloc(str.len);
+    for (int i=0; i<str.len; i++) {
+        data[i] = str.text[i];
+    }
+    return {.text=data, .len=str.len};
+}
+
 template<typename T>
 optional<T> ok(T val) { return {true, val}; }
 
 template<typename T>
 optional<T> not_ok() { return {false}; }
-
 
 optional<string> cwd_str() {
     char cwd[PATH_MAX];
@@ -46,6 +55,20 @@ string slice(string str, int start, int end) {
         .text=str.text+start,
         .len=(size_t)(end-start)
     };
+}
+
+string trim(string str) {
+    char ch;
+    while (ch = str.text[0], ch == ' ' || ch == '\n' || ch == '\t') {
+        str.text++;
+        str.len--;
+    }
+
+    while (ch = str.text[str.len-1], ch == ' ' || ch == '\n' || ch == '\t') {
+        str.len--;
+    }
+
+    return str;
 }
 
 void fill_charp(string str, char* charp) {
@@ -119,38 +142,78 @@ void charp_set(char* charp, const char* text, int at) {
     }
 }
 
-bool is_git_dir(string root) {
+optional<string> read_file(char* path) {
+    FILE* file = fopen(path, "r");
+    if (file == 0) return not_ok<string>();
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* mem = (char*)malloc(size);
+    auto res = fread(mem, size+1, 1, file);
+    return ok<string>({mem, size});
+}
+
+optional<string> git_root(string root) {
     int nth = 0;
     char path[PATH_MAX];
+    fill_charp(root, path);
     int idx = root.len;
 
     while (true) {
-        auto s = slice(root, 0, idx);
-        fill_charp(s, path);
-        charp_set(path, "/.git", s.len);
+        charp_set(path, "/.git", idx);
         if (dir_exists(path)) {
-            return true;
+            return ok(slice(root, 0, idx));
         }
 
         idx = index_of(root, '/', nth);
         if (idx == -1) break;
         nth--;
     }
-    return false;
+    return not_ok<string>();
+}
+
+optional<string> git_branch_name(string root) {
+    char path[PATH_MAX];
+    fill_charp(root, path);
+    charp_set(path, "/.git/HEAD", root.len);
+    path[root.len + sizeof("/.git/HEAD") - 1] = 0;
+
+    auto str = read_file(path);
+    if (!str.ok) { return str; }
+
+    auto idx = index_of(str.value, '/', -1);
+    if (idx == -1) { return not_ok<string>(); }
+
+    auto res = trim(slice(str.value, idx+1, str.value.len));
+    return ok(res);
+}
+
+optional<string> env_var(const char* name) {
+    auto val = getenv(name);
+    if (val == 0) return not_ok<string>();
+    return ok(to_string(val));
 }
 
 int main() {
     optional<string> opt_cwd = cwd_str();
     if (!opt_cwd.ok) return 0;
     auto cwd = opt_cwd.value;
-    auto frag = path_frag(cwd, -1, 0);
+    auto frag = path_frag(copy(cwd), -1, 0);
+    auto git_dir = git_root(copy(cwd));
+    auto branch = git_dir.ok ? git_branch_name(copy(git_dir.value)) : not_ok<string>();
+    auto venv = env_var("VIRTUAL_ENV");
+    auto last = env_var("?");
 
-    printf("%.*s\n", (int)frag.len, frag.text);
-    printf("%d\n", is_git_dir(cwd));
+    printf("Dir name: %.*s\n", (int)frag.len, frag.text);
+    if (git_dir.ok) {
+        printf("GIT: %.*s\n", git_dir.value.len, git_dir.value.text);
+    }
 
-    DIR *dp;
-    dirent *ep;
-    dp = opendir("./");
+    if (branch.ok) {
+        printf("BRANCH: %.*s\n", branch.value.len, branch.value.text);
+    }
 
-    if (dp == NULL) return 0;
+    if (venv.ok) {
+        printf("VENV: %.*s\n", venv.value.len, venv.value.text);
+    }
 }
